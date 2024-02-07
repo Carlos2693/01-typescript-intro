629f7fa6d797a01ca4bbe90d87ee266aa18c50fe
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2024-02-07 11:21:16 -0600

    12. Tipos de datos - continuación

--- src/bases/01-types.ts
+++ src/bases/01-types.ts
@@ -1,3 +1,16 @@
 export const name = 'Carlos'
 export const age: number = 30
 export const isValid: boolean = true
+
+export const templateString = ` Esto es un string
+multilinea
+que puede tener
+" dobles
+' simples
+inyectar valores ${ name }
+expresiones ${ 1 + 1}
+números: ${ age }
+booleanos: ${ isValid }
+`
+
+console.log(templateString);
